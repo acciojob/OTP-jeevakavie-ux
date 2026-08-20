10c8b6372ecf34c6fcb2a677524cf928a03ec1e2
jeevakavie-ux <jeevakavie@gmail.com>
2026-08-21 00:12:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes.forEach((code, index) => {
+
+    code.addEventListener("input", () => {
+        if (code.value.length > 0 && index < codes.length - 1) {
+            codes[index + 1].focus();
+        }
+    });
+
+    code.addEventListener("keydown", (e) => {
+
+        if (e.key === "Backspace") {
+
+            if (code.value === "" && index > 0) {
+                codes[index - 1].focus();
+            } else if (index > 0) {
+                code.value = "";
+                codes[index - 1].focus();
+            }
+        }
+    });
+});
